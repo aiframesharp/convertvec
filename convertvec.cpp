f019f4345f47eee6efbbb920037286e6ac5d694a
Zahar <ponimashz@mail.ru>
2020-12-11 01:04:43 +0300

    1) fix 2) only bin2txt

--- convertvec.cpp
+++ convertvec.cpp
@@ -1,6 +1,6 @@
 // Code to convert word2vec vectors between text and binary format
 // Created by Marek Rei
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -9,103 +9,50 @@
 
 const long long max_w = 2000;
 
-// Convert from text format to binary
-void txt2bin(char * input_path, char * output_path){
-	FILE** fi = 0;
-	fopen_s(fi, input_path, "rb");
-	FILE** fo = 0;
-	fopen_s(fo, output_path, "wb");
-
-	long long words, size;
-	fscanf_s(*fi, "%lld", &words);
-	fscanf_s(*fi, "%lld", &size);
-	fscanf_s(*fi, "%*[ ]");
-	fscanf_s(*fi, "%*[\n]");
-
-	fprintf(*fo, "%lld %lld\n", words, size);
-
-	char word[max_w];
-	char ch;
-	float value;
-	int b, a;
-	for (b = 0; b < words; b++) {
-		if(feof(*fi))
-			break;
-
-		word[0] = 0;
-		fscanf_s(*fi, "%[^ ]", word);
-		fscanf_s(*fi, "%c", &ch);
-		// This kind of whitespace handling is a bit more explicit than usual.
-		// It allows us to correctly handle special characters that would otherwise be skipped.
-
-		fprintf_s(*fo, "%s ", word);
-
-		for (a = 0; a < size; a++) {
-			fscanf_s(*fi, "%s", word);
-			fscanf_s(*fi, "%*[ ]");
-			value = atof(word);
-			fwrite(&value, sizeof(float), 1, *fo);
-		}
-		fscanf_s(*fi, "%*[\n]");
-		fprintf_s(*fo, "\n");
-	}
-	
-	fclose(*fi);
-	fclose(*fo);
-}
 
 // Convert from binary to text format
-void bin2txt(char * input_path, char * output_path){
-	FILE** fi = 0;
-	fopen_s(fi, input_path, "rb");
-	FILE** fo = 0;
-	fopen_s(fo, output_path, "wb");
+ void bin2txt(const char * input_path, const char * output_path){
 	
-	long long words, size;
-	fscanf_s(*fi, "%lld", &words);
-	fscanf_s(*fi, "%lld", &size);
-	fscanf_s(*fi, "%*[ ]");
-	fscanf_s(*fi, "%*[\n]");
+	FILE* fi = fopen(input_path, "rb");
+	FILE* fo = fopen(output_path, "wb");
 
-	fprintf_s(*fo, "%lld %lld\n", words, size);
+	long long words, size;
+	fscanf(fi, "%lld", &words);
+	fscanf(fi, "%lld", &size);
+	fscanf(fi, "%*[ ]");
+	fscanf(fi, "%*[\n]");
+
+	fprintf(fo, "%lld %lld\n", words, size);
 
 	char word[max_w];
 	char ch;
 	float value;
 	int b, a;
 	for (b = 0; b < words; b++) {
-		if(feof(*fi))
+		if (feof(fi))
 			break;
 
 		word[0] = 0;
-		fscanf_s(*fi, "%[^ ]", word);
-		fscanf_s(*fi, "%c", &ch);
-		
-		fprintf(*fo, "%s ", word);
+		fscanf(fi, "%[^ ]", word);
+		fscanf(fi, "%c", &ch);
+
+		fprintf(fo, "%s ", word);
 		for (a = 0; a < size; a++) {
-			fread(&value, sizeof(float), 1, *fi);
-			fprintf(*fo, "%lf ", value);
+			fread(&value, sizeof(float), 1, fi);
+			fprintf(fo, "%lf ", value);
 		}
-		fscanf_s(*fi, "%*[\n]");
-		fprintf_s(*fo, "\n");
+		fscanf(fi, "%*[\n]");
+		fprintf(fo, "\n");
 	}
-	fclose(*fi);
-	fclose(*fo);
+	fclose(fi);
+	fclose(fo);
 }
 
 int main(int argc, char **argv) {
-	if (argc < 4) {
-		printf("USAGE: convertvec method input_path output_path\n");
-		printf("Method is either bin2txt or txt2bin\n");
-		return 0;
-	}
-	
-	if(strcmp(argv[1], "bin2txt") == 0)
-		bin2txt(argv[2], argv[3]);
-	else if(strcmp(argv[1], "txt2bin") == 0)
-		txt2bin(argv[2], argv[3]);
-	else
-		printf("Unknown method: %s\n", argv[1]);
+
+	const char* inp = "input.bin";
+	const char* out = "outp.txt";
+	bin2txt(inp,out);
 
 	return 0;
 }
